5db2e591b71d88b73c6fbba191ffef41b25b564e
josedamss <jose_damast38@unicah.edu>
2026-07-18 21:47:17 -0600

    agregamos clase conexion y frmlogin

---
+++ SistemaRRHH/SistemaRRHH/DatosSQL/ConexionSQL.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRRHH.DatosSQL
+{
+    internal class ConexionSQL
+    {
+        private readonly string cadenaConexion = "Server=localhost; Port=3306; Database=sistemarrhh; Uid=root; Pwd=Holasoyjose123@;";
+
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error crítico al conectar a la Base de Datos:\n" + ex.Message, "Error de Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return conexion;
+        }
+    }
+}
+
